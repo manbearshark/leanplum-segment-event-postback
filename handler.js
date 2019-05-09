df933616478a4335eac9890b20fb6fe95222130e
ikrtolica <igor@blinkinc.com>
2019-05-09 13:17:10 -0700

    Message processing logic

--- handler.js
+++ handler.js
@@ -5,33 +5,42 @@ const axios = require('axios');
 const SEGMENT_URL = process.env.SEGMENT_URL;
 const SOURCE_KEY = process.env.SOURCE_KEY;
 
-module.exports.eventTransformer = async (event, context, callback) => {
-  const leanPlumEvent = JSON.parse(event.queryStringParameters)  // LP sends event data in QS params
+module.exports.lpEventTransformer = async (event, context, callback) => {
+
+  // LP Request format:
+  //
+  // [URL path to this Lambda]?message_id={{Message ID}}
+  //   %26event={{Message event}}%26device_id={{Device ID}}
+  //   %26userId={{User ID}}%26timestamp={{Trigger time}}
+  //   %26channel={{Message channel}}%26template_name={{Template name}}
+  //   %26parameters={{Parameters}}%26abTestID={{AB test ID}}
+  //   %26variantID={{Variant ID}}
+  //
+  // The code below assumes that this is the format that will be used to
+  // set up postbacks.
+  //
+  // This is intended to act as an endpoint for both AB Test events and for
+  // message events, as Lean Plum wants to get two separate endpoints for each
+  // for some reason
+
   let response = {
     statusCode: 200,
     body: null
-    }),
   };
 
-  console.log(leanPlumEvent);
-
-  try {
-    leanPlumEvent.timeStamp = new Date(leanPlumEvent.time).toISOString();  // LP sends Unix epoch time stamps in ms
-    leanPlumEvent.eventName = leanPlumEvent.channel + " " + leanPlumEvent.event;
-
-    if(!isBlank(leanPlumEvent.user_id)) {
-      leanPlumEvent.userId = leanPlumEvent.user_id;
-    }
-
-    if(!isBlank(leanPlumEvent.device_id)) {
-      leanPlumEvent.context = { device: { id: leanPlumEvent.device_id }};
-    }
-
-    await track(leanPlumEvent);
-  } catch (e) {
-    console.log(e);
+  let eventParams = mapToSegment(event.queryStringParameters);
+  if( eventParams == null) {
     response.statusCode = 500;
-    response.body = JSON.stringify({message: e.message});
+    response.body = "Could not parse event parameters.";
+  } else {
+    console.log(eventParams);
+    try {
+      track(eventParams);
+    } catch (e) {
+      console.log("ERROR - could not send event: ", e.message);
+      response.statusCode = 500;
+      response.body = e.message;
+    }
   }
 
   callback(null, response);
@@ -63,9 +72,51 @@ async function post(data, token, url) {
   }
 }
 
-async function track({eventName, properties, timestamp, userId, context}) {
+function mapToSegment(eventParams) {
   try {
-    await post({userId, properties, event: eventName, timestamp},
+    eventParams.properties = {};
+
+    // LP sends Unix epoch time stamps in ms
+    eventParams.timestamp = new Date(parseInt(eventParams.timestamp)).toISOString();
+
+    if(eventParams.abTestID) {
+      eventParams.event = "AB Test";
+      eventParams.properties.abTestID = eventParams.abTestID;
+      eventParams.properties.variantID = eventParams.variantID;
+    } else {
+      eventParams.event = eventParams.channel + " " + eventParams.event;
+    }
+
+    if(isBlank(eventParams.userId)) {
+      eventParams.userId = null;
+    }
+
+    if(!isBlank(eventParams.device_id)) {
+      eventParams.context = { device: { id: eventParams.device_id }};
+    } else {
+      eventParams.context = {};
+    }
+
+    if(!isBlank(eventParams.parameters)) {
+      // LP sends URL encoded JSON for the event parameters
+      let params = JSON.parse(decodeURIComponent(eventParams.parameters));
+      eventParams.properties = Object.assign(eventParams.properties, params);
+    }
+
+    return { timestamp: eventParams.timestamp,
+             event: eventParams.event,
+             userId: eventParams.userId,
+             context: eventParams.context,
+             properties: eventParams.properties };
+  } catch (e) {
+    console.log("ERROR - Could not map event properties: ", e.message);
+    return null;
+  }
+}
+
+async function track({event, properties, timestamp, userId, context}) {
+  try {
+    await post({userId, properties, event, timestamp, context},
       SOURCE_KEY, `${SEGMENT_URL}/track`);
   } catch (e) {
     throw e;
